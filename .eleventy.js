82ae08c036bb1d6aa3001d9d5796bc4c99b429b5
Peter deHaan <peter@deseloper.com>
2021-02-13 11:43:14 -0800

    Try this again

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,22 @@
 const { loremIpsum } = require("lorem-ipsum");
 
 module.exports = (eleventyConfig, defaultOpts = {}) => {
+  defaultOpts = Object.assign(
+    {},
+    { count: 1, units: "paragraphs", format: "html" },
+    defaultOpts
+  );
   const fn = lipsumFn(defaultOpts);
   eleventyConfig.addShortcode("lipsum", fn);
   eleventyConfig.addShortcode("lorem-ipsum", fn);
 };
 
 function lipsumFn(defaultOpts = {}) {
-  return (count = 1, units = "paragraphs", format = "html") => {
+  return (
+    count = defaultOpts.count,
+    units = defaultOpts.units,
+    format = defaultOpts.format
+  ) => {
     const lipsumOpts = Object.assign({}, defaultOpts, { count, units, format });
     return loremIpsum(lipsumOpts);
   };
